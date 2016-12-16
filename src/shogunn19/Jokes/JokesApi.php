cc3f5847ddba898d48514b5a8e3176330118e2ed
shogunn19 <shogunn19@gmail.com>
2016-12-16 18:58:05 +0100

    Edycja JokesApi.php

--- src/shogunn19/Jokes/JokesApi.php
+++ src/shogunn19/Jokes/JokesApi.php
@@ -1,41 +1,39 @@
 <?php
 namespace shogunn19\Jokes;
-
+//************************************************************************************************
 class JokesApi
 {
-const URL = 'http://ec2-35-156-131-145.eu-central-1.compute.amazonaws.com/list';
+const URL = 'http://ec2-35-156-131-145.eu-central-1.compute.amazonaws.com/list'; //url listy dowcipow
 
  $http; 
  $jokeTab;
 
-public function __construct($http)
-{
-     $this->http = $http;
-
-     $jokeTab = json_decode( $this->http->request('GET', self::URL)->getBody(), true );
-}
-
-public function randomJoke() //losowanie losowego zartu
-{ 
-	return $jokeTab[rand(0,9)][content];
-}
-
-
-public function all() //pobieranie wszystkich dowcipow
-{
-	for(int i=0;i<10;++i)
+	public function __construct($http) //konstruktor
 	{
-	  $jokesStr[i]=$jokeTab[i][content]
+	     $this->http = $http; //this.http = http
+
+	     $jokeTab = json_decode( $this->http->request('GET', self::URL)->getBody(), true );
+		//true -> konwersja do tablicy
 	}
-	return $jokesStr[];
+
+	public function randomJoke() //losowanie losowego zartu
+	{ 
+		return $jokeTab[rand(0,9)][content];
+	}
+
+
+	
+	public function all() //pobieranie wszystkich dowcipow
+	{
+		for(int i=0;i<10;++i)
+		{
+		  $jokesStr[i]=$jokeTab[i][content]
+		}
+		return $jokesStr[];
+	}
+
 }
-
-
-
-
-
-}
-
+//************************************************************************************************
 
 
 
